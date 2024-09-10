ba0c72fe584df3e917dcb69e800b2b60aa500c99
hnnsb <hannes.b@wtnet.de>
2024-09-10 21:39:12 +0200

    [REF] category drop down styling

--- web-app/src/components/category/category-card.tsx
+++ web-app/src/components/category/category-card.tsx
@@ -1,17 +1,24 @@
-import {Category} from "@/lib/model/category";
-import {FaPen, FaTrash} from "react-icons/fa";
+import { Category } from "@/lib/model/category";
+import { FaPen, FaTrash } from "react-icons/fa";
+import { Button } from "react-bootstrap";
 
 interface CategoryCardProps {
     category: Category;
     onDelete: any;
     onEdit: any;
-
 }
 
-export default function CategoryCard({category, onDelete, onEdit}: Readonly<CategoryCardProps>) {
-    return (<div>
-        <div style={{backgroundColor: category.color, width: '20px', height: '20px'}}></div>
-        <button onClick={onEdit}><FaPen/></button>
-        <button onClick={onDelete}><FaTrash/></button>
-    </div>);
+export default function CategoryCard({ category, onDelete, onEdit }: Readonly<CategoryCardProps>) {
+    return (
+        <div className="flex justify-between category-card">
+            <div className="flex mx-1 my-auto">
+                <div className="m-auto rounded-circle w-6 h-6" style={{ backgroundColor: category.color }}></div>
+                <p className="m-auto p-1 font-semibold">{category.name}</p>
+            </div>
+            <div className="flex my-auto category-card-buttons">
+                <Button className="p-2 m-1" onClick={onEdit}><FaPen /></Button>
+                <Button variant="danger" className="p-2 m-1" onClick={onDelete}><FaTrash /></Button>
+            </div>
+        </div>
+    );
 }
--- web-app/src/components/category/category-dropdown.tsx
+++ web-app/src/components/category/category-dropdown.tsx
@@ -1,7 +1,7 @@
 import Dropdown from 'react-bootstrap/Dropdown';
-import { DropdownButton } from 'react-bootstrap';
+import {DropdownButton} from 'react-bootstrap';
 import CategoryCard from '@/components/category/category-card';
-import { Category } from '@/lib/model/category';
+import {Category} from '@/lib/model/category';
 
 interface CategoryDropdownProps {
     categories: Category[];
@@ -14,14 +14,11 @@ export default function CategoryDropdown({categories, onEdit, onDelete}: Readonl
         <DropdownButton className="m-1" id="dropdown-basic" title="Edit Categories">
             {categories.map((category) => (
                 <Dropdown.Item key={category.id}>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <span>{category.name}</span>
-                        <CategoryCard
-                            category={category}
-                            onEdit={() => onEdit(category)}
-                            onDelete={() => onDelete(category)}
-                        />
-                    </div>
+                    <CategoryCard
+                        category={category}
+                        onEdit={() => onEdit(category)}
+                        onDelete={() => onDelete(category)}
+                    />
                 </Dropdown.Item>
             ))}
         </DropdownButton>
