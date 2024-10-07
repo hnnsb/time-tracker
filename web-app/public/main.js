6aa658b69cb2440dd105bc4ec066c365c892178f
hnnsb <hannes.b@wtnet.de>
2024-10-07 17:03:15 +0200

    auto update

--- web-app/public/main.js
+++ web-app/public/main.js
@@ -1,8 +1,21 @@
 const { BrowserWindow, app } = require("electron");
 const { join } = require("path");
+const { autoUpdater } = require("electron-updater");
+const log = require("electron-log");
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = "info";
+log.info("App starting...");
+
+let mainWindow;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send("message", text);
+}
 
 function createWindow() {
-  let mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {},
@@ -14,10 +27,34 @@ function createWindow() {
     ? mainWindow.loadFile(join(__dirname, "../build/index.html"))
     : mainWindow.loadURL("http://localhost:3000");
 
+  mainWindow.webContents.openDevTools();
+
   mainWindow.on("closed", () => (mainWindow = null));
+  return mainWindow;
 }
 
-app.on("ready", createWindow);
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", (info) => {
+  sendStatusToWindow("Update downloaded");
+});
+
+app.on("ready", function () {
+  createWindow();
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
@@ -29,6 +66,10 @@ app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+app.on("ready", function () {
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
 module.exports = app;
